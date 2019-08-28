99bc131b818b6d9d4eeefeb429f00495a8279485
Eric Drumond <ed9bh@users.noreply.github.com>
2019-08-28 17:32:38 -0300

    Add files via upload

--- 2019_Py_LSP/Py/qsave.py
+++ 2019_Py_LSP/Py/qsave.py
@@ -10,24 +10,24 @@ sleep(10)
 #model = doc.ModelSpace
 # %%
 count = 0
-final = 6
+final = 12
 WaitTime = 300
 while count is not final:
     now = str(datetime.now().hour) + ':' + str(datetime.now().minute)
     try:
         doc = acad.ActiveDocument
-        #doc.SetVariable("cmdecho", 0)
         doc.SendCommand('_QSAVE\n')
         print(f'Save {count} complete...{now}')
-        #doc.SetVariable("cmdecho", 1)
         doc = None
         pass
     except Exception as error:
         print(error)
         pass
     count += 1
-    if final != count:
+    if count != final:
         sleep(WaitTime)
+    else:
+        break
     pass
 print('Comando finalizado...')
 # (setq model(vla-get-modelspace(setq doc(vla-get-activedocument(setq cad(vlax-get-acad-object))))))
