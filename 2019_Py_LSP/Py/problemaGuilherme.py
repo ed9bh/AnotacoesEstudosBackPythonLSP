00cb56b8317b5810f8ca254f4390cdab7c1e95f8
ed9bh <Eric.Gomes@snclavalin.com>
2019-09-04 09:54:02 -0300

    0.0.2

--- 2019_Py_LSP/Py/problemaGuilherme.py
+++ 2019_Py_LSP/Py/problemaGuilherme.py
@@ -1,30 +1,81 @@
-#%%
-from numpy import pi, sin, cos, arccos as acos, abs
+# %%
+# Formula : https://pt.quora.com/Como-voc%C3%AA-calcula-a-dist%C3%A2ncia-entre-duas-localiza%C3%A7%C3%B5es-geogr%C3%A1ficas-expressas-em-coordenadas-de-latitude-e-longitude-usando-Python-ou-Java
+from numpy import pi, sin, cos, arccos as acos, abs, radians, arctan2 as atan2
 
-#%%
-
+# %%
+# Constantes
 rad = pi / 180
-bh = [-20,-45]
-cidade = ['Buenos Aires', 'Paris', 'Montreal', 'Tokio', 'Sydney', 'New York', 'Cape Town']
-lat = [-34.6,49.,45.,36.,-33.,40.,-33.]
-lon = [-58.4,4.,-72.,140.,151.,-74.,18.5]
+R = 6371.0
+
+# %%
+# Funcoes
+
+
+def Formula_001(lat1, lon1, lat2, lon2):
+    dlat = lat1 - lat2
+    dlon = lon1 - lon2
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(a**0.5, (1 - a)**0.5)
+    return R * c
+
+# %%
+
+
+baseCity = {
+    0: {
+        'Name': 'Belo Horizonte',
+        'Coords': {'lat': -20, 'lon': -45},
+    },
+}
+citys = {
+    0: {
+        'Name': 'Buenos Aires',
+        'Coords': {'lat': -34.6, 'lon': -58.4},
+    },
+    1: {
+        'Name': 'Paris',
+        'Coords': {'lat': 49.0, 'lon': 4},
+    },
+    2: {
+        'Name': 'Montreal',
+        'Coords': {'lat': 45, 'lon': -72},
+    },
+    3: {
+        'Name': 'Tokio',
+        'Coords': {'lat': 36, 'lon': 140},
+    },
+    4: {
+        'Name': 'Sydney',
+        'Coords': {'lat': -33, 'lon': 151},
+    },
+    5: {
+        'Name': 'New York',
+        'Coords': {'lat': 40, 'lon': -74},
+    },
+    6: {
+        'Name': 'Cape Town',
+        'Coords': {'lat': -33, 'lon': 18.5},
+    },
+}
 aaa = 0
 bbb = 0
 
-#%%
-def calcCCC(w, x, y, z):
-    global rad
-    return (sin(w * rad) * sin(y * rad)) + (cos(w * rad) * cos(y * rad)) * cos((x - z) * rad)
-#%%
+# %%
 
-for i in range(0, len(cidade)):
-    ccc = calcCCC(bh[0], bh[1], lat[i], lon[i])
-    ddd = acos(ccc) / rad
-    eee = ccc * ddd
+for i in baseCity:
+    BaseName = baseCity[i]['Name']
+    BaseLat = radians(baseCity[i]['Coords']['lat'])
+    BaseLon = radians(baseCity[i]['Coords']['lon'])
+    print(f'{BaseName} : {BaseLat} / {BaseLon}')
+    for j in citys:
+        Name = citys[j]['Name']
+        Lat = radians(citys[j]['Coords']['lat'])
+        Lon = radians(citys[j]['Coords']['lon'])
 
-    print(f'A distancia entre BH e {cidade[i]} é de {abs(eee):.3f}')
+        dist = Formula_001(BaseLat, BaseLon, Lat, Lon)
 
-    if eee > aaa:
-        aaa = eee
-    else:
-        bbb = eee
+        print('A distancia entre {0} e a cidade {1} é de {2:0.3f}'.format(
+            BaseName,
+            Name,
+            dist
+        ))
