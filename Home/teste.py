b8dc6052d42bf1487073f8577acc332925205623
ed9bh <ericdrumond@gmail.com>
2019-09-08 22:10:24 -0300

    v

--- Home/teste.py
+++ Home/teste.py
@@ -1,1 +1,13 @@
 #%%
+from time import sleep
+
+#%%
+c = 0
+f = 60
+while c < f:
+    print('\r[', end='')
+    print('/' * c, end='')
+    print(' ' * ((f - 1) - c), end='')
+    print(']', end='')
+    c += 1
+    sleep(1)
