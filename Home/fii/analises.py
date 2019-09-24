ef92e96282ea1fc394bae515814e71fd873595f8
ed9bh <ericdrumond@gmail.com>
2019-09-23 21:37:46 -0300

    v

--- Home/fii/analises.py
+++ Home/fii/analises.py
@@ -4,4 +4,8 @@ import matplotlib as plt
 
 
 #%%
-Lista_de_FII = pd.read_csv()
+Lista_de_FII = pd.read_csv('https://raw.githubusercontent.com/ed9bh/AnotacoesEstudosBackPythonLSP/master/Home/fii/Report.csv')
+# %%
+Lista_de_FII.head()
+
+#%%
