5336bc41214c58be673333e6d89500156372417d
ed9bh <ericdrumond@gmail.com>
2019-09-29 06:59:29 -0300

    v

--- Home/fii/fii.py
+++ Home/fii/fii.py
@@ -38,79 +38,77 @@ Rent_XPath = '//*[@id="dividends"]/div/div/div[2]/div[1]/div/div/table/tbody/tr[
 
 # %%
 
-for item in data_sheet['Ticker']:
-    start = perf_counter()
-    try:
-        ticker = f'{item}11.SA'
-
-        print(f'Começando {ticker}...', end='')
-        df = pdr.DataReader(ticker, data_source='yahoo', start='2019-01-01')
-
+if __name__ == '__main__':
+    for item in data_sheet['Ticker']:
+        start = perf_counter()
         try:
-            df.to_excel(Base_Dir + '/' + folder + '/' + ticker + '.xlsx')
-            pass
-        except:
-            pass
+            ticker = f'{item}11.SA'
 
-        # Media Movel Aritimetica
+            print(f'Começando {ticker}...', end='')
+            df = pdr.DataReader(
+                ticker, data_source='yahoo', start='2019-01-01')
 
-        df['MMA10'] = df['Adj Close'].rolling(10).mean()
-        df['MMA30'] = df['Adj Close'].rolling(30).mean()
-        df['MMA60'] = df['Adj Close'].rolling(60).mean()
+            # Media Movel Aritimetica
 
-        # Graficos
+            df['MMA10'] = df['Adj Close'].rolling(10).mean()
+            df['MMA30'] = df['Adj Close'].rolling(30).mean()
+            df['MMA60'] = df['Adj Close'].rolling(60).mean()
 
-        plt.plot(df['Adj Close'], color='Blue', label='Adj Close')
-        plt.plot(df['MMA10'], color='Yellow', label='MMA_10')
-        plt.plot(df['MMA30'], color='Orange', label='MMA_30')
-        plt.plot(df['MMA60'], color='Red', label='MMA_60')
+            df.to_csv(Base_Dir + '/' + folder + '/' + ticker + '.csv')
 
-        plt.gcf().autofmt_xdate()
-        date_format = mpl_dates.DateFormatter('%b, %d, %Y')
-        plt.gca().xaxis.set_major_formatter(date_format)
+            # Graficos
 
-        plt.legend()
+            plt.plot(df['Adj Close'], color='Blue', label='Adj Close')
+            plt.plot(df['MMA10'], color='Yellow', label='MMA_10')
+            plt.plot(df['MMA30'], color='Orange', label='MMA_30')
+            plt.plot(df['MMA60'], color='Red', label='MMA_60')
 
-        plt.title(f'Estudo do \"{ticker}\"')
+            plt.gcf().autofmt_xdate()
+            date_format = mpl_dates.DateFormatter('%b, %d, %Y')
+            plt.gca().xaxis.set_major_formatter(date_format)
 
-        plt.savefig(folder + '/' + ticker + '.png')
+            plt.legend()
 
-        plt.close()
+            plt.title(f'Estudo do \"{ticker}\"')
 
-        AluguelMes = '-'
+            plt.savefig(folder + '/' + ticker + '.png')
 
-        try:
-            page = requests.get(Web_Search_Link + ticker)
-            tree = html.fromstring(page.content)
-            AluguelMes = tree.xpath(Rent_XPath)[0].text
-            AluguelMes = AluguelMes.split(' ')
-            AluguelMes = AluguelMes[1]
-            AluguelMes = AluguelMes.replace(',', '.')
+            plt.close()
+
+            AluguelMes = '-'
+
+            try:
+                page = requests.get(Web_Search_Link + ticker)
+                tree = html.fromstring(page.content)
+                AluguelMes = tree.xpath(Rent_XPath)[0].text
+                AluguelMes = AluguelMes.split(' ')
+                AluguelMes = AluguelMes[1]
+                AluguelMes = AluguelMes.replace(',', '.')
+                pass
+            except Exception as error:
+                print(error)
+
+            with open(folder + '/Report.txt', 'a+') as ReportFile:
+                ReportFile.write(item + '11')
+                ReportFile.write('\t')
+                ReportFile.write(AluguelMes)
+                ReportFile.write('\t')
+                ReportFile.write(str(df['Adj Close'][-1]))
+                ReportFile.write('\n')
+
+            print('Finalizado...', end='')
+
+            sleep(randint(27, 45))
+
             pass
         except Exception as error:
             print(error)
+            sleep(randint(135, 270))
+            pass
 
-        with open(folder + '/Report.txt', 'a+') as ReportFile:
-            ReportFile.write(item + '11')
-            ReportFile.write('\t')
-            ReportFile.write(AluguelMes)
-            ReportFile.write('\t')
-            ReportFile.write(str(df['Adj Close'][-1]))
-            ReportFile.write('\n')
+        finish = perf_counter()
 
-        print('Finalizado...', end='')
-
-        sleep(randint(60, 70))
-
-        pass
-    except Exception as error:
-        print(error)
-        sleep(randint(250, 300))
-        pass
-
-    finish = perf_counter()
-
-    print(f'Processado em {finish - start:0.2f}...')
+        print(f'Processado em {finish - start:0.2f}...')
 
 # %%
 
