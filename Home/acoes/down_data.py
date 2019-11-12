16172b062f795beba1e4533a0448ee3918909801
ed9bh <Eric.Gomes@snclavalin.com>
2019-11-12 16:40:01 -0300

    v

---
+++ Home/acoes/down_data.py
@@ -0,0 +1,12 @@
+# %%
+from os import chdir
+import pandas_datareader as pdr
+from matplotlib import pyplot as plt
+import requests
+from lxml import html
+
+# %%
+chdir(r'C:\Users\GOMEE11\Documents\_Referencias\Git\AnotacoesEstudosBackPythonLSP\Home\acoes')
+file1 = r'.//Lista_Bovespa.csv'
+
+# %%
