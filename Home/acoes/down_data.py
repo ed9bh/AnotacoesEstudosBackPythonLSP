26286ac897f0878e7d94955cd901acf1df7a4b12
ed9bh <Eric.Gomes@snclavalin.com>
2019-11-13 11:08:04 -0300

    v

--- Home/acoes/down_data.py
+++ Home/acoes/down_data.py
@@ -1,12 +1,36 @@
 # %%
 from os import chdir
+from time import sleep, perf_counter
 import pandas_datareader as pdr
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, dates as mpl_dates
+import pandas as pd
+from pandas.plotting import register_matplotlib_converters
 import requests
 from lxml import html
 
+register_matplotlib_converters()
+plt.style.use('seaborn')
 # %%
-chdir(r'C:\Users\GOMEE11\Documents\_Referencias\Git\AnotacoesEstudosBackPythonLSP\Home\acoes')
-file1 = r'.//Lista_Bovespa.csv'
+def Down_Data(TickerName):
+    try:
+        Data = pdr.DataReader(TickerName + '.SA', data_source='yahoo', start='2019-01-01')
+        return Data
+    except:
+        return False
+
+def MMA_Analisys(Data):
+    Data['MMA200'] = Data['Adj Close'].rolling(200).mean()
+    Data['MMA100'] = Data['Adj Close'].rolling(100).mean()
+    Data['MMA72'] = Data['Adj Close'].rolling(72).mean()
+    Data['MMA66'] = Data['Adj Close'].rolling(66).mean()
+    Data['MMA21'] = Data['Adj Close'].rolling(21).mean()
+    return Data
+
+def Graph(Data):
+    pass
 
 # %%
+chdir(r'C:\Users\GOMEE11\Documents\_Referencias\Git\AnotacoesEstudosBackPythonLSP\Home\acoes')
+File_Lista_Acoes = r'.//Lista_Bovespa.csv'
+# %%
+DF_Tickers = pd.read_csv(File_Lista_Acoes)
