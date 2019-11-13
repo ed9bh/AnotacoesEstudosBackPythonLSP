04ab1d45d5d38bcfc6b356ab1e639aee2cb190b9
ed9bh <Eric.Gomes@snclavalin.com>
2019-11-13 12:04:39 -0300

    v

--- Home/acoes/down_data.py
+++ Home/acoes/down_data.py
@@ -1,5 +1,6 @@
 # %%
-from os import chdir
+from os import chdir, mkdir
+from os.path import isdir, isfile
 from time import sleep, perf_counter
 import pandas_datareader as pdr
 from matplotlib import pyplot as plt, dates as mpl_dates
@@ -11,12 +12,16 @@ from lxml import html
 register_matplotlib_converters()
 plt.style.use('seaborn')
 # %%
+
+
 def Down_Data(TickerName):
     try:
-        Data = pdr.DataReader(TickerName + '.SA', data_source='yahoo', start='2019-01-01')
+        Data = pdr.DataReader(TickerName + '.SA',
+                              data_source='yahoo', start='2019-01-01')
         return Data
     except:
         return False
+
 
 def MMA_Analisys(Data):
     Data['MMA200'] = Data['Adj Close'].rolling(200).mean()
@@ -26,11 +31,44 @@ def MMA_Analisys(Data):
     Data['MMA21'] = Data['Adj Close'].rolling(21).mean()
     return Data
 
-def Graph(Data):
+
+def Graph(Data, TickerName):
+    fig, ((A, B), (C, D)) = plt.subplots(
+        nrows=2,
+        ncols=2,
+        sharex=False,
+        sharey=False,
+        figsize=(15, 10)
+    )
+
+    A.plot(Data['Open'], color='Blue', label='Open')
+    A.plot(Data['Close'], color='Red', label='Close')
+    A.set_title(f'Graphic Open \\ Close {TickerName}')
+
+    B.plot(Data['Adj Close'], color='Black', label='Adj Close')
+    B.plot(Data['MMA72'], color='Red', label='72')
+    B.plot(Data['MMA21'], color='Orange', label='21')
+    B.plot(Data['MMA66'], color='Pink', label='66')
+    B.plot(Data['MMA100'], color='Grey', label='100')
+    B.plot(Data['MMA200'], color='Blue', label='200')
+    B.set_title(f'MMA VIEW {TickerName}')
+
+    D.bar(Data.index, Data['Volume'])
+    D.set_title(f'Volume - {TickerName}')
+
+    A.legend()
+    B.legend()
+
+    fig.autofmt_xdate()
+
+    fig.savefig(TickerName + '.png')
+
     pass
+
 
 # %%
 chdir(r'C:\Users\GOMEE11\Documents\_Referencias\Git\AnotacoesEstudosBackPythonLSP\Home\acoes')
 File_Lista_Acoes = r'.//Lista_Bovespa.csv'
+
 # %%
 DF_Tickers = pd.read_csv(File_Lista_Acoes)
