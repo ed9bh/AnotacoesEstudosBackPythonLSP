4f9e7f92999386950fbb9c95b6d1ec1bc35f278d
ed9bh <ericdrumond@gmail.com>
2019-11-16 11:00:11 -0300

    x

--- Home/acoes/make_data.py
+++ Home/acoes/make_data.py
@@ -19,25 +19,50 @@ plt.style.use('seaborn')
 
 
 def Down_Data(TickerName):
-    try:
-        Data = pdr.DataReader(TickerName + '.SA',
-                              data_source='yahoo', start='2019-01-01')
-        return Data
-    except:
-        return False
+    Data = pdr.DataReader(TickerName + '.SA',
+                          data_source='yahoo', start='2019-01-01')
+    return Data
 
 
 def MMA_Analisys(Data, Ticker):
+    # SMA
     Data['MMA200'] = Data['Adj Close'].rolling(200).mean()
     Data['MMA100'] = Data['Adj Close'].rolling(100).mean()
     Data['MMA72'] = Data['Adj Close'].rolling(72).mean()
     Data['MMA66'] = Data['Adj Close'].rolling(66).mean()
     Data['MMA21'] = Data['Adj Close'].rolling(21).mean()
+    Data['PriceX2'] = 2 * Data['Adj Close']
+
+    # Bollinger Bands
+    days = 9
+    MidBollinger = (Data['High'].rolling(days).mean() +
+                    Data['Low'].rolling(days).mean()) / 2
+    Data['CorridorHigh'] = MidBollinger + Data['PriceX2'].std()
+    Data['CorridorLow'] = MidBollinger - Data['PriceX2'].std()
+
+    # Pivot Points
+    global PP, R1, R2, R3, S1, S2, S3
+    ref = -1
+    PP = (Data['High'][ref] + Data['Low'][ref] + Data['Close'][ref]) / 3
+    R1 = (2 * PP) - Data['Low'][ref]
+    S1 = (2 * PP) - Data['High'][ref]
+    R2 = PP + (Data['High'][ref] - Data['Low'][ref])
+    S2 = PP - (Data['High'][ref] - Data['Low'][ref])
+    R3 = Data['High'][ref] + (2 * (PP - Data['Low'][ref]))
+    S3 = Data['Low'][ref] - (2 * (Data['High'][ref] - PP))
+
+    # Gain / Loss
+    Data['GainLoss'] = Data['Close'] - Data['Open']
+
+    table_file = '.\\Tables\\' + Ticker + '.xlsx'
+
+    Data.to_excel(table_file)
 
     return Data
 
 
 def Graph(Data, TickerName):
+
     fig, ((A, B), (C, D)) = plt.subplots(
         nrows=2,
         ncols=2,
@@ -48,6 +73,15 @@ def Graph(Data, TickerName):
 
     A.plot(Data['Open'], color='Blue', label='Open')
     A.plot(Data['Close'], color='Red', label='Close')
+    A.plot(Data['CorridorHigh'], color='Grey', linestyle=":")
+    A.plot(Data['CorridorLow'], color='Grey', linestyle=":")
+    first = True
+    for pivotLine in [PP, R1, R2, R3, S1, S2, S3]:
+        if first == True:
+            color, first = 'Black', False
+        else:
+            color = 'Grey'
+        A.axhline(pivotLine, color=color, linestyle="--")
     A.set_title(f'Graphic Open \\ Close {TickerName}')
 
     B.plot(Data['Adj Close'], color='Black', label='Adj Close')
@@ -57,6 +91,9 @@ def Graph(Data, TickerName):
     B.plot(Data['MMA100'], color='Grey', label='100')
     B.plot(Data['MMA200'], color='Blue', label='200')
     B.set_title(f'MMA VIEW {TickerName}')
+
+    C.bar(Data.index, Data['GainLoss'], color='Green')
+    C.set_title(f' Gain / Loss - {TickerName}')
 
     D.bar(Data.index, Data['Volume'])
     D.set_title(f'Volume - {TickerName}')
@@ -80,8 +117,10 @@ def Graph(Data, TickerName):
 
 
 # %%
-# chdir(r'C:\Users\GOMEE11\Documents\_Referencias\Git\AnotacoesEstudosBackPythonLSP\Home\acoes')
-chdir(r'A:\_Projetos\AnotacoesEstudosBackPythonLSP\Home\acoes')
+try:
+    chdir(r'A:\_Projetos\AnotacoesEstudosBackPythonLSP\Home\acoes')
+except:
+    chdir(r'C:\Users\GOMEE11\Documents\_Referencias\Git\AnotacoesEstudosBackPythonLSP\Home\acoes')
 
 File_Lista_Acoes = r'.//Lista_Bovespa.csv'
 File_List_Tickers = r'.//CheckPoint.txt'
@@ -91,48 +130,45 @@ Report = pd.DataFrame(columns=['Ticker', 'Adj Close'])
 List_Tickers = []
 mem = 0
 
-if isfile(File_List_Tickers):
-    with open(File_List_Tickers, 'r') as arq:
-        List_Tickers_Raw = arq.readlines()
-        for x in List_Tickers_Raw:
-            Ticker = x.replace('\n', '')
-            List_Tickers.append(Ticker)
+
+def inicializa():
+    global File_List_Tickers, File_Lista_Acoes, List_Tickers
+    if isfile(File_List_Tickers):
+        with open(File_List_Tickers, 'r') as arq:
+            List_Tickers_Raw = arq.readlines()
+            for x in List_Tickers_Raw:
+                Ticker = x.replace('\n', '')
+                List_Tickers.append(Ticker)
+            pass
         pass
-    pass
-else:
-    with open(File_Report, 'w+') as report:
-        report.write('Ticker,Price\n')
-    List_Tickers = list(DF_Tickers['Ticker'])
-    with open(File_List_Tickers, 'w+') as arq:
-        for x in List_Tickers:
-            arq.write(x + '\n')
+    else:
+        with open(File_Report, 'w+') as report:
+            report.write('Ticker,Price\n')
+            pass
+
+        List_Tickers = list(DF_Tickers['Ticker'])
+
+        with open(File_List_Tickers, 'w+') as arq:
+            for x in List_Tickers:
+                arq.write(x + '\n')
+            pass
         pass
-    pass
 
-
-if isdir('.\\Graph'):
-    pass
-    #to_erase = glob('.\\Graph\\*.png')
-    # for e in to_erase:
-    #     remove(e)
-    pass
-else:
-    mkdir('.\\Graph')
-    pass
-
-if isdir('.\\Tables'):
-    pass
-    #to_erase = glob('.\\Tables\\*.xlsx')
-    # for e in to_erase:
-    #     remove(e)
-else:
-    mkdir('.\\Tables')
-    pass
+    for d in ['.\\Graph', '.\\Tables']:
+        if isdir(d):
+            pass
+        else:
+            mkdir(d)
+            pass
 
 # %%
 
+
 if __name__ == '__main__':
     start = perf_counter()
+
+    inicializa()
+
     for x in List_Tickers:
 
         print(f'Downloading {x} data from yahoo...', end='')
@@ -142,7 +178,9 @@ if __name__ == '__main__':
             print(' Download Sucess...', end='')
             pass
         except:
+            data = None
             print(' Download Failed...', end='')
+            sleep(9)
             pass
         try:
             data = MMA_Analisys(data, x)
@@ -150,49 +188,35 @@ if __name__ == '__main__':
             pass
         except:
             print(' Analysis Failed...', end='')
+            sleep(9)
             pass
         try:
             Graph(data, x)
-            print(' Graph Sucess...')
+            print(' Graph Sucess...', end='')
             pass
         except:
-            print(' Graph Failed...')
-            pass
-
-        table_file = '.\\Tables\\' + x + '.xlsx'
-
-        if isfile(table_file):
-            remove(table_file)
-            sleep(1)
-
-        try:
-            data.to_excel(table_file)
-        except Exception as error:
-            print(error)
+            print(' Graph Failed...', end='')
+            sleep(9)
             pass
 
         try:
             with open(File_Report, 'a+') as report:
                 last_price = data['Adj Close'][-1]
                 report.write(f'{x},{last_price}\n')
-            # Report = Report.append(
-            #     {'Ticker': x, 'Adj Close': data['Adj Close'][-1]},  ignore_index=True)
-            # Report.to_excel('Report.xlsx')
         except Exception as error:
-            print(error)
+            # print(error)
+            sleep(9)
             pass
 
-        # if isfile('.\\Graph\\' + x + '.png'):
-        #     sleep(1)
         List_Tickers.remove(x)
         with open(File_List_Tickers, 'w+') as arq:
             for x in List_Tickers:
                 arq.write(x + '\n')
 
-        if mem == 9:
+        if mem == 18:
             collect()
             mem = 0
-            sleep(18)
+            sleep(45)
         else:
             mem += 1
             sleep(9)
