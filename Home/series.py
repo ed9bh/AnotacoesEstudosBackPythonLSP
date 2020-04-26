0a1070f18a57a0523790d351a5a71fa9e46c9d17
ed9bh <ericdrumond@gmail.com>
2020-04-26 19:04:03 -0300

    Redefinicao

--- Home/series.py
+++ Home/series.py
@@ -1,459 +1,189 @@
-# %%
-# Modulos
-from os import chdir, mkdir, remove, rename, walk, listdir
-from os.path import isfile, isdir
-from shutil import move, copyfile
+#%%
+from os import chdir, mkdir, remove, rename, listdir, walk, system
+from os.path import isdir, isfile
 from glob import glob
-from time import sleep
-from datetime import datetime
 from zipfile import ZipFile
-from re import findall, search
-from json import load, dump
 from rarfile import RarFile
-# %%
-# Diretorios
-BaseDownloadFolder = r'C:\Users\ericd\Downloads\Filmes\Series'
-BaseTorrentToDownload = r'\\DNS-320\P2P\Torrent'
-BaseCompletDownload = r'\\DNS-320\P2P\Complete'
-BaseFirstPermanentBackup = r'\\DNS-320\Volume_1\VIDEOS\Series'
-BaseSecondKeep = r'A:\Series'
-BaseIncomplete = r'\\DNS-320\P2P\incomplete'
-# %%
-# Variaveis
-srt_files, tor_files, interess_files = None, None, None
-checkpoint_file = 'checkpoint.json'
-checkpoint_serie = {}
-# %%
-# Funções
+from re import findall, search
+from time import sleep
+#%%
 
+class Color():
+    system('color')
+    
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-def locateBase():
-    chdir(BaseDownloadFolder)
-    print('Listando arquivos...')
-    global srt_files, tor_files, interess_files
-    try:
-        srt_files = glob('*.srt')
-        tor_files = glob('*.torrent')
-        srt_files.sort()
-        tor_files.sort()
-        interess_files = srt_files + tor_files
-        pass
-    except:
-        pass
-    sleep(3)
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
     pass
 
+def Translate_Name(raw_name:str):
+    
+    file_type = findall('\.[a-zA-Z0-9]{3,7}', raw_name)[-1][1:]
 
-def unzipFiles():
-    print('Descompactando arquivos...')
-    zip_files = glob('*.zip')
-    rar_files = glob('*.rar')
-    for z in zip_files:
-        with ZipFile(z) as target:
-            target.extractall()
+    name = raw_name.replace('.' + file_type, '')
+    name = name.upper()
+    
+    se_ep = findall('S\d[0-9]E\d[0-9]', name)[0]
+    season = findall('S\d[0-9]', name)
+    epsode = findall('E\d[0-9]', name)
+
+    name_list = name.split('.')
+    temp_list = []
+    final_name = ''
+
+    for item in name_list:
+        if item == se_ep:
+            break
+        else:
+            temp_list.append(item)
             pass
         pass
-    for r in rar_files:
-        with RarFile(r) as target:
-            target.extractall()
+    for item in temp_list:
+        final_name += item + ' '
         pass
-    sleep(9)
-    for root, folder, arch in walk(BaseDownloadFolder):
-        chdir(root)
-        arqs = glob('*.srt') + glob('*.torrent')
-        for k in arqs:
-            print('Arquivo Encontrado : ' + k)
-            try:
-                move(root + '\\' + k, BaseDownloadFolder)
-            except:
+    title = final_name[0:-1]
+    se_ep = se_ep.replace('E', '_E')
+    final_name = title + '_' + se_ep
+
+    return final_name, title, season[0], epsode[0], file_type
+
+def XCopy(_xname, *destinations):
+    with open(_xname, mode='rb') as ofile:
+        result = Translate_Name(_xname)
+        data = ofile.read()
+
+        print(f'{Color.YELLOW}---> 0.00% <---')
+
+        for c, item in enumerate(destinations):
+            name = f'{item}\\{result[0]}.{result[4]}'
+            name = name.title()
+            with open(name, 'wb+') as dfile:
+                dfile.write(data)
                 pass
+            print(f'{Color.YELLOW}---> {(100 / len(destinations) * (c + 1)):.2f}% <---')
             pass
         pass
-    chdir(BaseDownloadFolder)
-    sleep(60)
-    pass
+    return True
 
+#%%
+folders = {
+    'Download' : 'C:\\Users\\ericd\\Downloads\\Filmes\\Series',
+    'Torrent' : '\\\\DNS-320\\P2P\\Torrent',
+    'Complete' : '\\\\DNS-320\\P2P\\Complete',
+    'A_Series' : 'A:\\Series',
+    'X_Series' : '\\\\DNS-320\\Volume_1\\VIDEOS\\Series'
+}
+#%%
+_dir = folders['Download']
 
-def checkpoint():
-    global srt_files, tor_files
-    chdir(BaseDownloadFolder)
-    print('Criando Checkpoint...')
-    count = 0
-    for s, t in zip(srt_files, tor_files):
-        print(s)
-        print(t)
-        if s.replace('.srt', '') == t.replace('.torrent', ''):
-            raw_name = s.replace('.srt', '')
-            se_ep = findall('S\d[0-9]E\d[0-9]', raw_name)[0]
-            season = findall('S\d[0-9]', raw_name)
-            epsode = findall('E\d[0-9]', raw_name)
-            list_name = raw_name.split('.')
-            list_temp = []
-            final_name = ''
-            for i in list_name:
-                if i == se_ep:
-                    break
-                else:
-                    list_temp.append(i)
-                    pass
-                pass
-            for j in list_temp:
-                final_name += j + ' '
-                pass
-            title = final_name[0:-1]
-            se_ep = se_ep.replace('E', '_E')
-            final_name = title + '_' + se_ep
+chdir(_dir)
 
-            reg = str(count)
+_zfiles = glob('*.zip')
+_zfiles.extend(glob('*.rar'))
 
-            print(reg)
-
-            checkpoint_serie[reg] = {}
-            checkpoint_serie[reg]['Title'] = title
-            checkpoint_serie[reg]['Season'] = season[0]
-            checkpoint_serie[reg]['Episode'] = epsode[0]
-            checkpoint_serie[reg]['raw_name'] = raw_name
-            checkpoint_serie[reg]['OriginalTorName'] = t
-            checkpoint_serie[reg]['OriginalSrtName'] = s
-            checkpoint_serie[reg]['NewVidName'] = final_name
-            checkpoint_serie[reg]['NewSrtName'] = final_name + '.srt'
-
-            count += 1
+for item in sorted(_zfiles):
+    
+    if item.endswith('zip'):
+        with ZipFile(item, 'r') as zfile:
+            zfile.extractall()
             pass
-        pass
-    with open(checkpoint_file, 'w+') as CHECK:
-        dump(checkpoint_serie, CHECK)
-        pass
-    sleep(3)
-    pass
-
-
-def garbage():
-    global interess_files
-    print('Removendo o lixo...')
-    for f in interess_files:
-        try:
-            if search('.1080p.', f) or search('.720p.', f):
-                remove(f)
-                pass
+    elif  item.endswith('rar'):
+        with RarFile(item, 'r') as rfile:
+            rfile.extractall()
             pass
-        except Exception as error:
-            print(error)
-    pass
 
+    _del_files = glob('*.1080p.*')
+    _del_files.extend(glob('*.2160p.*'))
+    _del_files.extend(glob('*.720p.*'))
+    _del_files
 
-def deleteline_DICT_JSON(x):
-    global checkpoint_serie
-    print('Deletando o registro bem sucedido...')
-    del(checkpoint_serie[x])
-    with open(checkpoint_file, 'w') as target:
-        dump(checkpoint_serie, target)
+    for x in _del_files:
+        remove(x)
+        print(f'{Color.RED}O arquivo {x} foi deletado...')
         pass
-    pass
 
+    for x in glob('*.torrent'):
+        XCopy(x, folders['Torrent'])
+        print(f'{Color.GREEN}O arquivo {x} foi movido...')
+        remove(x)
+        pass
 
-def load_checkpoint():
-    global checkpoint_serie
-    print('Carregando Checkpoint Existente...')
-    with open(checkpoint_file, 'r') as target:
-        checkpoint_serie = load(target)
-    pass
+    for x in glob('*.srt'):
+        result = Translate_Name(x)
 
+        dest_1 = folders['X_Series'] + '\\' + result[1]
+        dest_2 = folders['A_Series'] + '\\' + result[1]
 
-def folderCreation(reg):
-    global BaseFirstPermanentBackup
-    TitleDirStorage = BaseFirstPermanentBackup + \
-        '\\' + checkpoint_serie[reg]['Title']
-    SeasonDirStorage = TitleDirStorage + '\\' + checkpoint_serie[reg]['Season']
-    TitleDir = BaseSecondKeep + '\\' + checkpoint_serie[reg]['Title']
-    SeasonDir = TitleDir + '\\' + checkpoint_serie[reg]['Season']
-    for d in [TitleDirStorage, TitleDir, SeasonDirStorage, SeasonDir]:
-        if isdir(d) == False:
-            print('Criando diretorio...')
+        for d in [dest_1, dest_2]:
             try:
                 mkdir(d)
-                pass
-            except Exception as error:
-                print(error)
-            pass
-        pass
-    pass
-
-
-def srtMoveCopy(reg):
-    vidOriginalName = checkpoint_serie[reg]['OriginalSrtName']
-    title = checkpoint_serie[reg]['Title']
-    season = checkpoint_serie[reg]['Season']
-    episode = checkpoint_serie[reg]['NewVidName']
-    ext = 'srt'
-
-    origin = '{0}\\{1}'.format(
-        BaseDownloadFolder,
-        vidOriginalName
-    )
-
-    destinyStorage = '{0}\\{1}\\{2}\\{3}.{4}'.format(
-        BaseFirstPermanentBackup,
-        title,
-        season,
-        episode,
-        ext
-    )
-
-    destiny = '{0}\\{1}\\{2}\\{3}.{4}'.format(
-        BaseSecondKeep,
-        title,
-        season,
-        episode,
-        ext
-    )
-    # Copia legenda...
-    print(f'Copiando : {destiny}')
-    try:
-        copyfile(origin, destiny)
-        pass
-    except Exception as error:
-        print('Erro ocorrido : ', str(error))
-        pass
-    sleep(3)
-    # Move legenda...
-    print(f'Copiando : {destinyStorage}')
-    try:
-        move(origin, destinyStorage)
-        pass
-    except Exception as error:
-        print('Erro ocorrido : ', str(error))
-    sleep(3)
-    pass
-
-
-def torMove(reg):
-    origin = '{0}\\{1}'.format(
-        BaseDownloadFolder,
-        checkpoint_serie[reg]['OriginalTorName']
-    )
-    destiny = '{0}\\{1}'.format(
-        BaseTorrentToDownload,
-        checkpoint_serie[reg]['OriginalTorName']
-    )
-    print(f'Iniciando torrente : {destiny}')
-    try:
-        move(origin, destiny)
-        pass
-    except Exception as error:
-        print('Erro ocorrido : ', str(error))
-        pass
-    sleep(3)
-    pass
-
-
-def vidMoveCopy(reg, folder):
-    vidOriginalName = checkpoint_serie[reg]['OriginalVidName']
-    vidOriginalName = vidOriginalName.replace('.torrent', '')
-    title = checkpoint_serie[reg]['Title']
-    season = checkpoint_serie[reg]['Season']
-    episode = checkpoint_serie[reg]['NewVidName']
-
-    origin = '{0}\\{1}'.format(
-        folder,
-        vidOriginalName
-    )
-
-    destinyStorage = '{0}\\{1}\\{2}\\{3}'.format(
-        BaseFirstPermanentBackup,
-        title,
-        season,
-        episode
-    )
-
-    destiny = '{0}\\{1}\\{2}\\{3}'.format(
-        BaseSecondKeep,
-        title,
-        season,
-        episode
-    )
-
-    # Copia video...
-    try:
-        print('Copiando para pasta Series...')
-        print(f'... {destiny} ...')
-        print(f'Iniciado em {datetime.now()}')
-        copyfile(origin, destiny)
-        print(f'Finalizado em {datetime.now()}')
-        pass
-    except Exception as error:
-        print('Erro ocorrido : ', str(error))
-        pass
-    sleep(3)
-    # Move video...
-    try:
-        print('Copiando para Backup...')
-        print(f'... {destinyStorage} ...')
-        print(f'Iniciado em {datetime.now()}')
-        move(origin, destinyStorage)
-        print(f'Finalizado em {datetime.now()}')
-        pass
-    except Exception as error:
-        print('Erro ocorrido : ', str(error))
-    sleep(3)
-    pass
-
-    pass
-
-
-def clearMess(x):
-    print('Apagando Checkpoint...')
-    if x == True:
-        remove(BaseDownloadFolder + '\\' + checkpoint_file)
-    else:
-        delList = glob(BaseDownloadFolder + '\\' + '*.*')
-        for erase in delList:
-            try:
-                remove(erase)
-                pass
-            except Exception as error:
-                print(error)
-            pass
-    pass
-
-
-def playlist(reg):
-    day = datetime.now().day
-    month = datetime.now().month
-    year = datetime.now().year
-
-    title = checkpoint_serie[reg]['Title']
-    season = checkpoint_serie[reg]['Season']
-    episode = checkpoint_serie[reg]['NewVidName']
-    vidFile = f'{title}/{season}/{episode}'
-
-    plName = f'{year}_{month:02d}_{day:02d}-TV.m3u'
-
-    print(f'Criando Playtist : ... {plName} ...')
-
-    arq = BaseSecondKeep + '\\' + plName
-
-    with open(arq, 'a+') as target:
-        target.write(vidFile)
-        target.write('\n')
-        pass
-    pass
-
-
-def realignName():
-    corrigir = glob(pathname='**/*.*', recursive=True)
-    errados = ['Nine.Nine', '.2018.']
-    corretos = ['Nine-Nine', '.']
-    for k in corrigir:
-        for e, c in zip(errados, corretos):
-            new = k.replace(e, c)
-            try:
-                rename(k, new)
-                pass
             except:
                 pass
 
+        dest_1 = dest_1 + '\\' + result[2]
+        dest_2 = dest_2 + '\\' + result[2]
 
-def directDownload():
-    print('Não implementado...')
+        for d in [dest_1, dest_2]:
+            try:
+                mkdir(d)
+                print(f'{Color.CYAN}O Diretorio {d} foi criado...')
+            except:
+                pass
+
+        XCopy(x, dest_1, dest_2)
+        print(f'{Color.OKGREEN}A Legenda {result[0]} foi copiada para \n\t{dest_1}\n\t{dest_2}')
+
+        remove(x)
     pass
 
+# %%
+for folder, b, c in walk(folders['Complete']):
+
+    chdir(folder)
+
+    destine = folders['A_Series']
+    backup = folders['X_Series']
+    copiado = False
+
+    video_files = []
+    for extension in ('mp4', 'mkv', 'avi', 'mov'):
+        video_files.extend(glob(f'*.{extension}'))
+
+    for item in sorted(video_files):
+        try:
+            result = Translate_Name(item)
+            dest = f'{destine}\\{result[1]}\\{result[2]}'
+            dest_2 = f'{backup}\\{result[1]}\\{result[2]}'
+            print(f'{Color.WHITE}Copiando : {Color.CYAN}{result[0]}')
+            copiado = XCopy(item, dest, dest_2)
+            print(f'{Color.WHITE}Arquivo copiado para {Color.OKGREEN}\n\t{dest}\n\t{dest_2}')
+            pass
+        except Exception as er:
+            print(f'{Color.WHITE}Erro : {Color.FAIL}{er}')
+        
+        if copiado == True:
+            try:
+                remove(item)
+            except Exception as er:
+                print(f'{Color.WHITE}Erro : {Color.FAIL}{er}')
 
 # %%
-# Programa
-if __name__ == '__main__':
-
-    # %%
-    # Acessa pasta principal
-    print(f'Progresso {datetime.now()}')
-    chdir(BaseDownloadFolder)
-    # %%
-    # Descompacta arquivos
-    print(f'Progresso {datetime.now()}')
-    unzipFiles()
-    # Localiza arquivos de interesse
-    # %%
-    print(f'Progresso {datetime.now()}')
-    locateBase()
-    # %%
-    # Limpa arquivos indesejaveis
-    print(f'Progresso {datetime.now()}')
-    garbage()
-    # %%
-    chdir(BaseDownloadFolder)
-    realignName()
-    # %%
-    # Atualiza lista de arquivos
-    print(f'Progresso {datetime.now()}')
-    locateBase()
-    # %%
-    # Cria ou Carrega o cronograma de downloads
-    print(f'Progresso {datetime.now()}')
-    chdir(BaseDownloadFolder)
-    if isfile(BaseDownloadFolder + '\\' + checkpoint_file):
-        #print('Carregando Checkpoint...')
-        reStart = True
-        load_checkpoint()
-        pass
-    else:
-        #print('Criando Checkpoint...')
-        checkpoint()
-        reStart = False
-        pass
-    for item in checkpoint_serie:
-        print(item)
-        print('Criando estrutura para {0}'.format(
-            checkpoint_serie[item]['NewVidName']))
-        folderCreation(item)
-        torMove(item)
-        srtMoveCopy(item)
-        pass
-    # %%
-    # Move os Videos para a pasta correta...
-    print(f'Progresso {datetime.now()}')
-    sleep(600 * len(checkpoint_serie))
-    # %%
-    # Tratamento para nomes errados
-    print(f'Progresso {datetime.now()}')
-
-    chdir(BaseCompletDownload)
-    realignName()
-
-    # %%
-    done = []
-    for item in checkpoint_serie:
-        vidExt = ['.mp4', '.mkv', '.avi']
-        vidName = checkpoint_serie[item]['OriginalSrtName']
-        vidName = vidName.replace('.srt', '')
-        vidNewName = checkpoint_serie[item]['NewVidName']
-        for root, folder, arq in walk(BaseCompletDownload):
-            for i in vidExt:
-                if isfile(root + '\\' + vidName + i) and not vidName.endswith('.mp4'):
-                    checkpoint_serie[item]['OriginalVidName'] = vidName + i
-                    checkpoint_serie[item]['NewVidName'] = vidNewName + i
-                    print(checkpoint_serie[item]['OriginalVidName'])
-                    print(checkpoint_serie[item]['NewVidName'])
-                    pass
-                pass
-            try:
-                if isfile(root + '\\' + checkpoint_serie[item]['OriginalVidName']):
-                    vidMoveCopy(item, root)
-                    playlist(item)
-                    done.append(item)
-                    pass
-                pass
-            except Exception as error:
-                print('Erro : Arquivo pode já ter sido copiado ou (' + str(error) + ')')
-            pass
-        pass
-    for item in checkpoint_serie:
-        playlist(item)
-        pass
-    for d in done:
-        chdir(BaseDownloadFolder)
-        deleteline_DICT_JSON(d)
-    print(f'Progresso {datetime.now()}')
-    if reStart == True:
-        clearMess(True)
-    elif reStart == False:
-        clearMess(False)
+print(f'{Color.END}')
+print('\n' * 9)
+print('Processo finalizado...')
+print('\n' * 9)
+sleep(99)
