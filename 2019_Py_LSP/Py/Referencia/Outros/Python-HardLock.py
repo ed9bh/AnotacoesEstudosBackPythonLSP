376017973f239179f2b593779196cba214bd40d5
Eric Drumond <ed9bh@users.noreply.github.com>
2022-07-28 15:50:22 -0300

    Update Python-HardLock.py

--- 2019_Py_LSP/Py/Referencia/Outros/Python-HardLock.py
+++ 2019_Py_LSP/Py/Referencia/Outros/Python-HardLock.py
@@ -4,10 +4,10 @@ for l in map(chr, range(97, 123)):
     print(l)
     try:
         serial = win32api.GetVolumeInformation(l + ':/')
-        print(serial)
+        print(f'Serial : {serial}')
         if serial[1] == 1616992295:
             print('Serial encontrado com sucesso...')
             break
         pass
     except Exception as error:
-        print('Erro : ' + str(error))
+        print(f'Erro : {str(error)}')
